072c307fd5dbcc605dd72e7b61b600a93952966c
Dan Bracey <penguingeek1@gmail.com>
2024-03-10 17:40:17 +0000

    Remove media query requirement by using Tailwind CSS Responsive design principles

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -9,11 +9,11 @@ import EnvelopeIcon from "@/components/icons/EnvelopeIcon.vue";
 </script>
 
 <template>
-<nav class="flex bg-black text-white" style="height: 100%; text-align: center">
-  <RouterLink to="/home" class="flex-auto">Home</RouterLink>
-  <RouterLink to="/services" class="flex-auto">Landscaping Services</RouterLink>
-  <RouterLink to="/gallery" class="flex-auto">Gallery</RouterLink>
-  <RouterLink to="/faq" class="flex-auto">FAQ</RouterLink>
-  <RouterLink to="/contact-us" class="flex-auto">Contact Us</RouterLink>
+<nav class="flex flex-col sm:flex-row bg-black text-white bg-origin-padding p-4 " style="height: 100%; text-align: center">
+  <RouterLink to="/home" class="flex-auto amaranth-regular sm:text-3xl">Home</RouterLink>
+  <RouterLink to="/services" class="flex-auto amaranth-regular sm:text-3xl">Landscaping Services</RouterLink>
+  <RouterLink to="/gallery" class="flex-auto amaranth-regular sm:text-3xl">Gallery</RouterLink>
+  <RouterLink to="/faq" class="flex-auto amaranth-regular sm:text-3xl">FAQ</RouterLink>
+  <RouterLink to="/contact-us" class="flex-auto amaranth-regular sm:text-3xl">Contact Us</RouterLink>
 </nav>
 </template>
