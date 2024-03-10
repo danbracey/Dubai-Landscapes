a7047c57fd8e6b23d8a85a0e9266b39914c72dac
Dan Bracey <penguingeek1@gmail.com>
2024-03-10 22:25:21 +0000

    Add hero image - Need to fix CSS Grid on mobile

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -4,11 +4,9 @@ import EnvelopeIcon from "@/components/icons/EnvelopeIcon.vue";
 </script>
 
 <template>
-<section class="text-white" style="height: 100%; text-align: center">
+<section class="grid content-center justify-items-center text-white img_9804 text-center align-middle h-1/2 space-y-4 items-center" style="height: 22.813vw; min-height: 20vw">
   <!-- 9804 -->
-  <div class="block img_9804 h-3/5" style="height: 400px">
-    <p>UAE's leading landscapes</p>
-    <button class="bg-black border-5">View our Gallery</button>
-  </div>
+  <p class="text-4xl sm:text-4xl md:text-7xl tauri-regular" style="-webkit-text-stroke: 3px black;">UAE's leading landscapes</p>
+  <button class="bg-black border-5 rounded-full sm:text-2xl md:text-3xl p-5 border-4 border-olive mb-8 w-1/6"><RouterLink :to="{name: 'gallery'}">View our Gallery</RouterLink></button>
 </section>
 </template>
