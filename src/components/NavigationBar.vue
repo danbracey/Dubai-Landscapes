68a6b46fb4c79fdadee72dc355b645eb2f50648e
Dan Bracey <penguingeek1@gmail.com>
2024-03-10 17:00:58 +0000

    Refactor index.html to increase semantic tag usage

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+// defineProps<{
+//   phone?: String,
+//   email?: String
+// }>()
+import uk_flag from '../assets/uk_flag.webp';
+import EnvelopeIcon from "@/components/icons/EnvelopeIcon.vue";
+</script>
+
+<template>
+<nav class="flex bg-black text-white" style="height: 100%; text-align: center">
+  <RouterLink to="/home" class="flex-auto">Home</RouterLink>
+  <RouterLink to="/services" class="flex-auto">Landscaping Services</RouterLink>
+  <RouterLink to="/gallery" class="flex-auto">Gallery</RouterLink>
+  <RouterLink to="/faq" class="flex-auto">FAQ</RouterLink>
+  <RouterLink to="/contact-us" class="flex-auto">Contact Us</RouterLink>
+</nav>
+</template>
