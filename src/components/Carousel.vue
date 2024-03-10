7ea41320269375a85682249b7f0050e12a739272
Dan Bracey <penguingeek1@gmail.com>
2024-03-10 18:57:48 +0000

    Compress client provided images into webp

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,29 +1,14 @@
 <script setup lang="ts">
-
-
-import PhoneIcon from "@/components/icons/PhoneIcon.vue";
-
-defineProps<{
-  phone?: String,
-  email?: String
-}>()
 import uk_flag from '../assets/uk_flag.webp';
 import EnvelopeIcon from "@/components/icons/EnvelopeIcon.vue";
 </script>
 
 <template>
-<section class="flex bg-olive text-white" style="height: 100%; text-align: center">
-  <span class="basis-1/3 text-base hidden sm:block">
-    <img :src="uk_flag" alt="Flag of the United Kingdom" class="british inline rounded-full">
-    British company
-  </span>
-  <span class="sm:basis-3/3 text-base self-center content-center basis-full" id="phone_number">
-    <PhoneIcon width="30"></PhoneIcon>
-    {{phone}}
-  </span>
-  <span class="basis-1/3 text-base self-center hidden sm:block">
-    <EnvelopeIcon width="30"></EnvelopeIcon>
-    {{email}}
-  </span>
+<section class="text-white" style="height: 100%; text-align: center">
+  <!-- 9804 -->
+  <div class="block img_9804 h-3/5" style="height: 400px">
+    <p>UAE's leading landscapes</p>
+    <button class="bg-black border-5">View our Gallery</button>
+  </div>
 </section>
 </template>
--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -10,10 +10,10 @@ import EnvelopeIcon from "@/components/icons/EnvelopeIcon.vue";
 
 <template>
 <nav class="flex flex-col sm:flex-row bg-black text-white bg-origin-padding p-4 " style="height: 100%; text-align: center">
-  <RouterLink to="/home" class="flex-auto amaranth-regular sm:text-3xl">Home</RouterLink>
-  <RouterLink to="/services" class="flex-auto amaranth-regular sm:text-3xl">Landscaping Services</RouterLink>
-  <RouterLink to="/gallery" class="flex-auto amaranth-regular sm:text-3xl">Gallery</RouterLink>
-  <RouterLink to="/faq" class="flex-auto amaranth-regular sm:text-3xl">FAQ</RouterLink>
-  <RouterLink to="/contact-us" class="flex-auto amaranth-regular sm:text-3xl">Contact Us</RouterLink>
+  <RouterLink :to="{name: 'home'}" class="flex-auto amaranth-regular sm:text-3xl">Home</RouterLink>
+  <RouterLink :to="{name: 'services'}" class="flex-auto amaranth-regular sm:text-3xl">Landscaping Services</RouterLink>
+  <RouterLink :to="{name: 'gallery'}" class="flex-auto amaranth-regular sm:text-3xl">Gallery</RouterLink>
+  <RouterLink :to="{name: 'faq'}" class="flex-auto amaranth-regular sm:text-3xl">FAQ</RouterLink>
+  <RouterLink :to="{name: 'contact-us'}" class="flex-auto amaranth-regular sm:text-3xl">Contact Us</RouterLink>
 </nav>
 </template>
