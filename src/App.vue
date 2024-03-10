af1652fe22dd222be9d836b12ddfcb511e748a5b
Dan Bracey <penguingeek1@gmail.com>
2024-03-10 15:58:23 +0000

    Ensure ContactBanner displays on every page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import ContactBanner from "@/components/ContactBanner.vue";
 </script>
 
 <template>
-  <RouterView />
+  <header>
+    <ContactBanner phone="0118 999 881 999 119 7253" email="customer-services@dubai-landscapes.com"></ContactBanner>
+  </header>
+  <main>
+    <RouterView />
+  </main>
 </template>
 
 <style src="./assets/tailwind.css"></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,6 @@
 <script setup lang="ts">
-
-import ContactBanner from "@/components/ContactBanner.vue";
 </script>
 
 <template>
-  <header>
-    <ContactBanner phone="0118 999 881 999 119 7253" email="customer-services@dubai-landscapes.com"></ContactBanner>
-  </header>
-  <main>
 
-  </main>
 </template>
