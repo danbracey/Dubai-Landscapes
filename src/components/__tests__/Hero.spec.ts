6d5c58b9dda52526ad4964479e501751f812d5bd
Dan Bracey <penguingeek1@gmail.com>
2024-03-11 15:16:32 +0000

    Write Hero unit tests

---
+++ src/components/__tests__/Hero.spec.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import Hero from "../Hero.vue";
+
+describe('Hero section', () => {
+    const wrapper = mount(Hero, {
+        props: {
+            h1: 'Landscaping Services',
+            subtitle: 'For your personal oasis in the UAE'
+        }
+    })
+
+    it('h1 renders properly', () => {
+        expect(wrapper.props("h1")).toBe('Landscaping Services')
+    })
+    it('subtitle renders properly', () => {
+        expect(wrapper.props("subtitle")).toBe('For your personal oasis in the UAE')
+    })
+})
