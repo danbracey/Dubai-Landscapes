4498248e19a1d19b43ff524f83af9d3e68e58afd
Dan Bracey <penguingeek1@gmail.com>
2024-03-11 15:18:05 +0000

    Create Hero initial file structure

---
+++ src/components/Hero.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  h1?: String,
+  subtitle?: String
+}>()
+</script>
+
+<template>
+<section class="flex bg-grass text-white" style="height: 100%; text-align: center">
+  <h1>{{h1}}</h1>
+  <i>{{subtitle}}</i>
+</section>
+</template>
