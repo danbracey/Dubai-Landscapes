ea19298f94beceec07c12b8e3969c14efcf6ae75
Dan Bracey <24969761@edgehill.ac.uk>
2024-03-13 13:11:34 +0000

    Refactor CSS, Remove unused declarations

--- src/components/CarouselBanner.vue
+++ src/components/CarouselBanner.vue
@@ -11,7 +11,7 @@ defineProps<{
 
 <template>
   <!-- grid -->
-<section class="grid text-white img_9804 text-center align-middle h-1/2 space-y-4 place-items-center border-olive border-b-8" style="height: 22.813vw; min-height: 20vw" id="carousel">
+<section class="grid text-white bg-center text-center align-middle h-1/2 space-y-4 place-items-center border-olive border-b-8" style="height: 22.813vw; min-height: 20vw" id="carousel">
   <!-- 9804 -->
   <p class="text-4xl sm:text-4xl md:text-7xl tauri-regular" style="-webkit-text-stroke: 3px black;">{{banner_text}}</p>
   <button class="bg-black border-5 rounded-full sm:text-2xl md:text-3xl p-2 sm:p-5 border-4 border-olive mb-8"><RouterLink :to="{name: route_name}">{{ button_text }}</RouterLink></button>
