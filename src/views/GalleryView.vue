8ba5012814e03fd529f36ae2db6c4a997c80ed18
Dan Bracey <24969761@edgehill.ac.uk>
2024-03-13 11:59:30 +0000

    Fix dynamic image imports in Gallery for build process

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -6,7 +6,7 @@ import TextBlock from "@/components/TextBlock.vue";
 
 <template>
   <Hero h1="Our Projects" subtitle="Grab a glimpse of our work"></Hero>
-  <TextBlock background_img="IMG_9804.webp">
+  <TextBlock :style="{ 'background-image': `url(${IMG_9804})` }">
     Over the years, the team here at Dubai-Landscapes has worked on a whole host of projects throughout the UAE,
     helping to enhance the outdoor space of numerous clients in the region. While we could tell you about our best work,
     we thought it would be better to show you instead. On this page, you’ll find our projects gallery where you can
@@ -14,17 +14,32 @@ import TextBlock from "@/components/TextBlock.vue";
   </TextBlock>
   <GreenBanner>Projects Gallery</GreenBanner>
   <section class="grid grid-cols-6 text-center text-3xl md:text-6xl tauri-regular text-white grid-height text-outline" id="services-grid">
-    <div class="relative grid-height" style="background-image: url('/src/assets/IMG_9772.webp');"></div>
-    <div class="relative grid-height" style="background-image: url('/src/assets/IMG_9413.webp');"></div>
-    <div class="relative grid-height" style="background-image: url('/src/assets/IMG_9648.webp');"></div>
-    <div class="relative grid-height" style="background-image: url('/src/assets/016A3065.webp');"></div>
-    <div class="relative grid-height" style="background-image: url('/src/assets/016A3298.webp');"></div>
-    <div class="relative grid-height" style="background-image: url('/src/assets/016A3170.webp');"></div>
-    <div class="relative grid-height" style="background-image: url('/src/assets/IMG_9700.webp');"></div>
-    <div class="relative grid-height" style="background-image: url('/src/assets/IMG_9373.webp');"></div>
-    <div class="relative grid-height" style="background-image: url('/src/assets/CM3_4146.webp');"></div>
-    <div class="relative grid-height" style="background-image: url('/src/assets/IMG_9582.webp');"></div>
-    <div class="relative grid-height" style="background-image: url('/src/assets/016A3289.webp');"></div>
-    <div class="relative grid-height" style="background-image: url('/src/assets/IMG_9725.webp');"></div>
+    <div class="relative grid-height" :style="{ 'background-image': `url(${IMG_9804})` }"></div>
+    <div class="relative grid-height" :style="{ 'background-image': `url(${IMG_9413})` }"></div>
+    <div class="relative grid-height" :style="{ 'background-image': `url(${IMG_9648})` }"></div>
+    <div class="relative grid-height" :style="{ 'background-image': `url(${_016A3065})` }"></div>
+    <div class="relative grid-height" :style="{ 'background-image': `url(${_016A3298})` }"></div>
+    <div class="relative grid-height" :style="{ 'background-image': `url(${_016A3170})` }"></div>
+    <div class="relative grid-height" :style="{ 'background-image': `url(${IMG_9700})` }"></div>
+    <div class="relative grid-height" :style="{ 'background-image': `url(${IMG_9373})` }"></div>
+    <div class="relative grid-height" :style="{ 'background-image': `url(${CM3_4146})` }"></div>
+    <div class="relative grid-height" :style="{ 'background-image': `url(${IMG_9582})` }"></div>
+    <div class="relative grid-height" :style="{ 'background-image': `url(${_016A3289})` }"></div>
+    <div class="relative grid-height" :style="{ 'background-image': `url(${IMG_9725})` }"></div>
   </section>
 </template>
+
+<script lang="ts">
+import IMG_9804 from '@/assets/IMG_9804.webp';
+import IMG_9413 from '@/assets/IMG_9413.webp';
+import IMG_9648 from '@/assets/IMG_9648.webp';
+import _016A3065 from '@/assets/016A3065.webp';
+import _016A3298 from '@/assets/016A3298.webp';
+import _016A3170 from '@/assets/016A3170.webp';
+import IMG_9700 from '@/assets/IMG_9700.webp';
+import IMG_9373 from '@/assets/IMG_9373.webp';
+import CM3_4146 from '@/assets/CM3_4146.webp';
+import IMG_9582 from '@/assets/IMG_9582.webp';
+import _016A3289 from '@/assets/016A3289.webp';
+import IMG_9725 from '@/assets/IMG_9725.webp';
+</script>
